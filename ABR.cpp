7ccfff1eaa71c8ffc2a44b3ce61674238fa63636
Julie <julie.etienne@insa-lyon.fr>
2017-01-09 15:20:35 +0100

    Création fichier ABR.cpp

---
+++ ABR.cpp
@@ -0,0 +1,1 @@
+#include "ABR.h"
--- ABR.h
+++ ABR.h
@@ -1,3 +1,12 @@
+//Constructeurs:
+//Par défaut, avec param pour la clé, avec param pour fg et fr
+//Destructeur
+
 class ABR {
+
+  private:
+    int* key;
+    ABR* fg;
+    ABR* fr;
 
 };
